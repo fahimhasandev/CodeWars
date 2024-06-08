8b63dbd0454d66178692a00d77943f9254fe6502
macAir <macair@macAirs-Air.lan>
2024-06-08 16:44:01 -0500

    pushing summation problem

--- 8 kyu/Grasshopper - Summation.js
+++ 8 kyu/Grasshopper - Summation.js
@@ -4,7 +4,29 @@
 // For example (Input -> Output):
 // 2 -> 3 (1 + 2)
 // 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
-//P--->
-//R
-//E
-//P
+
+//if the number takes 2 --> 1 + 2 = 3
+
+//Parameter---> number
+//Return --> sum of the number starting from 1 to that (N)Number
+//Example --> 1 -3 + 4 --> 5
+//Psudo code-->
+// 1. check the number is positive or negative
+// 2. it will start from 1 up to the number --> n + 1
+// 3. conditional
+// 4  loop
+// 4. log
+
+const summation = function (num) {
+  // Code here
+  if (num > 0) {
+    let count = 0;
+    for (let i = 1; i <= num; i++) {
+      count = count + i;
+    }
+    return count;
+  }
+};
+
+console.log(summation(-4) === 10);
+console.log(summation(4) === 10);
