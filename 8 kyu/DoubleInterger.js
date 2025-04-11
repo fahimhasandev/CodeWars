937ae0fdf6df1828fa8b5a010ea8ce39c6dbcf1d
fahimhasandev <fhasan5@wgu.edu>
2025-04-11 10:06:12 -0500

    double Interger Problem

---
+++ 8 kyu/DoubleInterger.js
@@ -0,0 +1,6 @@
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return parseInt(i * i);
+}
+console.log(doubleInteger(5)); // Output: 25
+console.log(doubleInteger(-2)); // Output: 4
